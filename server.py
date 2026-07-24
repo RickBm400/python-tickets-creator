2ec8b92d3240478fe7fe6ff1b5ae2453b4b6f8a5
RickBm400 <rjbm29@gmail.com>
2026-07-23 22:14:14 -0500

    feat: practice endpoint

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return { "message": "Hello World" }
+
+
+@app.get("/greetins")
+def greetings(name: Optional[str] = None):
+    try:
+        if not name:
+            raise ValueError("Not valid param")
+        return {
+            "message": "Hello " + name
+        }
+    except ValueError as error:
+        print("Error: ", error)
+        raise HTTPException(status_code=404, detail= {
+            "message": str(error),
+            "code": "400"
+        })
