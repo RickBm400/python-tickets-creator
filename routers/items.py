475d5a960212f150cdb05b4e6229a8dab91e0973
RickBm400 <rjbm29@gmail.com>
2026-08-17 13:50:44 -0500

    add pylint rules

--- routers/items.py
+++ routers/items.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter, HTTPException
 from typing import Optional
+from fastapi import APIRouter, HTTPException, status
 
 router = APIRouter(
     prefix="",
     tags=["items"],
     dependencies=[],
-    responses={ 404: { "description": "Not found" } }
+    responses={404: {"description": "Not found"}},
 )
+
 
 @router.get("/")
 async def root():
-    return { "message": "Hello World" }
+    return {"message": "Hello World"}
 
 
 @router.get("/greetins")
@@ -18,12 +19,9 @@ def greetings(name: Optional[str] = None):
     try:
         if not name:
             raise ValueError("Not valid param")
-        return {
-            "message": "Hello " + name
-        }
-    except ValueError as error:
-        print("Error: ", error)
-        raise HTTPException(status_code=404, detail= {
-            "message": str(error),
-            "code": "400"
-        })
+        return {"message": "Hello " + name}
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"message": "not creted", "code": "400"},
+        ) from e
